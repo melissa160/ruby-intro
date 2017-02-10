41b4f0b372354f1c82c5a9adbd79e9fd067f3cc7
melissa160 <melissa160@gmail.com>
2017-02-09 19:16:45 -0500

    solucion al reto 6

--- challenges/5-format-address/my_solution.rb
+++ challenges/5-format-address/my_solution.rb
@@ -2,5 +2,5 @@
 
 # Tu solucion abajo:
 def make_address(street, city, state, zip)
-	"Tu vives en #{state} en la hermosa ciudad de #{city} calle #{street} y tu zip es #{zip}"
+	"You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
 end
--- challenges/6-math-methods/my_solution.rb
+++ challenges/6-math-methods/my_solution.rb
@@ -1,6 +1,20 @@
 # Math Methods
 
 # Tu solucion abajo:
+def add(num1, num2)
+	num1 + num2
+end
 
+def subtract(num1, num2)
+	num1 - num2
+end
+
+def multiply(num1, num2)
+	num1 * num2
+end
+
+def divide(num1, num2)
+	num1.to_f / num2
+end
 
 
