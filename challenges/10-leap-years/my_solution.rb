692f1e7a913f7c3179e187a778145069ed760480
melissa160 <melissa160@gmail.com>
2017-02-10 18:54:53 -0500

    solucion hasta el 11

--- challenges/10-leap-years/my_solution.rb
+++ challenges/10-leap-years/my_solution.rb
@@ -4,4 +4,25 @@
 
 
 # Tu solucion abajo:
+def leap_year?(año)
+#	if año%4 == 0 && año%100 != 0
+#		true
+#	elsif año%4 == 0 && año%400 == 0
+#		true
+#	else 
+#		false
+#	end
 
+#    año%400==0 && !(año%100==0) && año%4 == 0
+
+	if año%400==0 
+		true
+	elsif año%100==0
+		false
+	elsif año%4 == 0
+		true
+	else
+		false
+	end
+
+end
