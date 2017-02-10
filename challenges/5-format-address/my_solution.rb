48f556c3f54fe5ffaeab9381ea5d09af013e2ed3
melissa160 <melissa160@gmail.com>
2017-02-09 19:08:30 -0500

    solucion al 5 reto

--- challenges/5-format-address/my_solution.rb
+++ challenges/5-format-address/my_solution.rb
@@ -1,3 +1,6 @@
 # Format an Address
 
 # Tu solucion abajo:
+def make_address(street, city, state, zip)
+	"Tu vives en #{state} en la hermosa ciudad de #{city} calle #{street} y tu zip es #{zip}"
+end
