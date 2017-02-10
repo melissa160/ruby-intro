6cfecd11ffad0a1e972b5eb968eccd73c905ebe5
melissa160 <melissa160@gmail.com>
2017-02-09 22:55:03 -0500

    solucionando el reto 9

--- challenges/8-calculate-grade/my_solution.rb
+++ challenges/8-calculate-grade/my_solution.rb
@@ -1,4 +1,17 @@
 # Calculate a Grade
 
 # Tu solucion abajo:
+def get_grade(prom)
+	if prom < 60
+		"F"
+	elsif (prom >= 60 && prom < 70)
+		"D"
+	elsif  (prom >= 70 && prom < 80)
+		"C"
+	elsif (prom >= 80 && prom < 90)
+		"B"
+	else
+		"A"
+	end
+end
 
--- challenges/9-triangle-side/my_solution.rb
+++ challenges/9-triangle-side/my_solution.rb
@@ -1,5 +1,23 @@
 # Triangle Side
 
 # Tu solucion abajo:
+def valid_triangle?(num1, num2, num3)
 
+	arr = [num1, num2, num3]
 
+	max_side = arr.max
+	sum_sides = num1+num2+num3
+
+	#validar si todos los lados son positivos
+	if arr.min <= 0
+		false
+	else
+		#evaluar si es un triangulo valido
+		if max_side < sum_sides - max_side
+		  	true
+		else 
+			false
+		end
+	end
+
+end
