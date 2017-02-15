32489f6fdc9308404d16d35497c2aa9c5441ea2e
melissa160 <melissa160@gmail.com>
2017-02-15 18:10:06 -0500

    ejercicio 19

--- challenges/19-pretty-triangle/my_solution.rb
+++ challenges/19-pretty-triangle/my_solution.rb
@@ -1,13 +1,24 @@
-# Pretty Triangle
+# # Pretty Triangle
 
 
-# Tu solucion abajo:
-def print_triangle(num)
-	(1..num).each do |x|
-		print "*"*x + "\n"
-	end
+# # Tu solucion abajo:
+# def print_triangle(num)
+# 	(1..num).each do |x|
+# 		print "*"*x + "\n"
+# 	end
+# end
+
+# #print_triangle(2)
+# print_triangle(5)
+# #print_triangle(0)
+
+def print_triangle(arg)
+    array = (1..arg).to_a
+    dot = []
+    array.map do |num|
+       dot.push("*" *  num)
+    end
+    puts dot
 end
 
-#print_triangle(2)
-print_triangle(5)
-#print_triangle(0)
+print_triangle(6)
