772f59f4a5dce1dba147817efacebfe42770f4f3
melissa160 <melissa160@gmail.com>
2017-03-02 18:32:55 -0500

    27 validat credit card

--- challenges/27-OOP/design-argument-order/my_solution.rb
+++ challenges/27-OOP/design-argument-order/my_solution.rb
@@ -1,16 +1,18 @@
+require "pry"
 class House
   attr_reader :square_feet, :num_bedrooms, :num_baths, :cost
+  attr_accessor :address
 
-  def initialize(address, square_feet, num_bedrooms = 3, num_baths = 2, cost = 320_000, down_payment = 0.20, sold = false, has_tenants = false)
-    @address = address
-    @square_feet = square_feet
-    @num_bedrooms = num_bedrooms
-    @num_baths = num_baths
-    @cost = cost
-    @down_payment = down_payment
-    @sold = sold
-    @short_sale = short_sale
-    @has_tenants = has_tenants
+  def initialize(house_feature = {})
+    @address = house_feature[:address] || "742 Evergreen Terrace"
+    @square_feet = house_feature[:square_feet] || "n/a"
+    @num_bedrooms = house_feature[:num_bedrooms] || 1
+    @num_baths = house_feature[:num_baths] || 1
+    @cost = house_feature[:cost] || 10000
+    @down_payment = house_feature[:down_payment] || 0.2
+    @sold = house_feature.fetch(:sold) { false }
+    @short_sale = house_feature[:short_sale] || "n/a"
+    @has_tenants = house_feature.fetch(:has_tenants) { false }
   end
 
   def obscure_address
@@ -18,7 +20,8 @@ class House
   end
 
   def buy!(money, good_credit)
-    @sold = true if money >= down_payment && good_credit
+    p money
+    @sold = true if money >= @down_payment && good_credit
   end
 
   def down_payment
@@ -28,8 +31,10 @@ class House
   def to_s
     "#{obscure_address} : #{square_feet} sq. ft., #{num_bedrooms} bed, #{num_baths} bath. $#{cost}"
   end
+  binding.pry
 end
 
 
 # Pon driver code aca para probar que todo este funcionando.
 ###### DRIVER CODE ########
+my_house = House.new({num_bedrooms: 3, num_baths: 2, cost: 320_000, down_payment: 0.20})
