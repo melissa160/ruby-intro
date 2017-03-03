cd2362b6c52819ea0f9eda83f90c2a83fbf90cdb
melissa <melissa160@gmail.com>
2017-03-03 11:56:26 -0500

    pad an array

--- challenges/21-pad-an-array/my_solution.rb
+++ challenges/21-pad-an-array/my_solution.rb
@@ -1,24 +1,24 @@
 # # Mi solucion:
 # def pad(arr, min, relleno=nil)
-# 	# arr_aux = arr.clone
-# 	# if arr_aux.length < min 
-# 	# 	(min-arr_aux.length).times {arr_aux.push(relleno)}
-# 	# end
-# 	# arr_aux
-# 	arr_aux =[]
-# 	arr_aux.push(arr)
-# 	if arr.length < min
-# 		(min-arr.length).times {arr_aux.push(relleno)}
-# 	end
-# 	arr_aux
-	
+#   # arr_aux = arr.clone
+#   # if arr_aux.length < min 
+#   #   (min-arr_aux.length).times {arr_aux.push(relleno)}
+#   # end
+#   # arr_aux
+#   arr_aux =[]
+#   arr_aux.push(arr)
+#   if arr.length < min
+#       (min-arr.length).times {arr_aux.push(relleno)}
+#   end
+#   arr_aux
+    
 # end
 
 # def pad!(arr, min, relleno=nil)
-# 	if arr.length < min 
-# 		(min-arr.length).times {arr.push(relleno)}
-# 	end
-# 	arr
+#   if arr.length < min 
+#       (min-arr.length).times {arr.push(relleno)}
+#   end
+#   arr
 # end
 
 # pad([1,2,3], 6)
@@ -26,64 +26,64 @@
 # p pad([1,2,3], 1, "beso")
 
 def my_map(array)
-	result = []
-	#array.drop_while {|element| result << yield(element)}
-	(self.array.length).times do |index|
-		result << yield(self[index])
-	end
-	result
+    result = []
+    #array.drop_while {|element| result << yield(element)}
+    (self.array.length).times do |index|
+        result << yield(self[index])
+    end
+    result
 
 end
 
 #p my_map([1,2,3,4]) {|n| n*2}
 
 def my_filter(array)
-	result = []
-	array.each do |element|
-		if (yield(element))
-			result << element
-		end
-	end
-	result
+    result = []
+    array.each do |element|
+        if (yield(element))
+            result << element
+        end
+    end
+    result
 end
 
 #p my_filter([1,2,3,4,5,6,7,8]) {|n| n%2 == 0}
 class Array
-	def my_find
-		var = nil
-		self.each do |element|
-			if yield(element)
-				var = element
-			end
-		end
-		var
-	end
+    def my_find
+        var = nil
+        self.each do |element|
+            if yield(element)
+                var = element
+            end
+        end
+        var
+    end
 
-	def my_find_all
-		var = []
-		self.each do |element|
-			if yield(element)
-				var << element
-			end
-		end
-		var
-	end
+    def my_find_all
+        var = []
+        self.each do |element|
+            if yield(element)
+                var << element
+            end
+        end
+        var
+    end
 
-	def my_inject(acu)
-		var = acu
-		self.each do |x|
-			var = yield(var,x)
-		end
-		var
-	end
+    def my_inject(acu)
+        var = acu
+        self.each do |x|
+            var = yield(var,x)
+        end
+        var
+    end
 
-	def my_min
-		self.my_inject(self.first) {|acu,x| x<acu ?   x : acu}		
-	end
-	
-	def my_max
+    def my_min
+        self.my_inject(self.first) {|acu,x| x<acu ?   x : acu}      
+    end
+    
+    def my_max
 
-	end
+    end
 end
 #p [1,2,3,4].my_inject({}){|acu,n| acu[n]=n; acu} //devuelvo acu al final de la linea porque acu es el hash
 #p [30,2,3,4].my_find_all {|x| x > 2}
@@ -91,25 +91,25 @@ end
 p [9,2,3,4,5,60].my_min
 
 def pad!(arr, num, word=nil)
-	if num <= arr.length
-		arr
-	else
-		(num-arr.length).times {arr << word}
-	end
-	arr
+    if num <= arr.length
+        arr
+    else
+        (num-arr.length).times {arr << word}
+    end
+    arr
 end
 
 def pad(arr, num, word=nil)
-	new_arr = []
-	new_arr << arr
-	new_arr.flatten!
-	if num <= new_arr.length
-		new_arr
-	else
-		(num-new_arr.length).times {new_arr << word}
-	end
-	new_arr
-	
+    new_arr = []
+    new_arr << arr
+    new_arr.flatten!
+    if num <= new_arr.length
+        new_arr
+    else
+        (num-new_arr.length).times {new_arr << word}
+    end
+    new_arr
+    
 end
 pad([1,2,3], 5, 'apple')
 
